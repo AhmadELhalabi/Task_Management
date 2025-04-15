3741057839aae48d4a78de02bcf724849ae512fa
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-04-15 21:12:08 +0300

    write the code of the constructor

--- scripts/BankAccount.js
+++ scripts/BankAccount.js
@@ -1,6 +1,8 @@
 class BankAccount {
     constructor(ownerName, initialBalance) {
-      
+        this.ownerName = ownerName;          // O(1)
+        this.balance = initialBalance;       // O(1)
+        this.history = [];                   // O(1)
       }
     
     deposit(amount) {
