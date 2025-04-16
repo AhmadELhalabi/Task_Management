909b51835a87c5242ee9473e655e694a658d9076
AhmadElhalabi <A.EL_HALABI@HOTMAI.com>
2025-04-16 22:33:21 +0300

    task label

--- scripts/Tasklist.js
+++ scripts/Tasklist.js
@@ -1,3 +1,21 @@
 const addBtn = document.querySelector('.add-btn');
 const taskInput = document.querySelector('.task-input');
 const taskList = document.querySelector('.task-list');
+
+function addTask() {
+    const taskText = taskInput.value.trim();
+  
+    if (taskText !== '') {
+        console.log('New Task:', taskText);
+    
+       //container div for the task and buttons 
+        const container = document.createElement('div');
+        container.classList.add('task-container'); 
+    
+        // task label
+        const taskLabel = document.createElement('span');
+        taskLabel.textContent = taskText;
+        taskLabel.classList.add('task-label');
+    
+      }
+    }
